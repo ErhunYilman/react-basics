17ee09c3bfc730a215316c0167f66cb4d8aa6045
Erhun Akbas <Erhun@perronwei.nl>
2019-05-19 20:35:01 +0200

    first component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,19 @@
-console.log("It works!");
+import React from "react";
+import { render } from "react-dom";
+
+class App extends React.Component {
+	render() {
+		return(
+		    // je kan hier geen geen meerdere elementen terug gevenn
+		    <div className="container">
+                <div className="row">
+                    <div className="col-sm">
+                        <h1>HTML in JavaScript whaaat??</h1>
+                    </div>
+                </div>
+			</div>
+		)
+	}
+}
+
+render (<App/>, window.document.getElementById("app"))
