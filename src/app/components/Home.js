60d820d09530b4406edeac181a6c985a3971b121
Erhun Akbas <Erhun@perronwei.nl>
2019-05-20 05:43:59 +0200

    dynamic data

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -3,9 +3,11 @@
  // leg iets uit warm je hier geen render gebruikt
  export class Home extends React.Component {
     render() {
+        // Als je meer complexte code wilt schijjven en dit later in de component wilt uit lezen moet je die code hier schijven
         return(
             <div>
                 <p>dit komt vanaf het Home component</p>
+                {/*kan maar op eem lijn dynamic  data uitvoeren */}
             </div>
         )
     }
